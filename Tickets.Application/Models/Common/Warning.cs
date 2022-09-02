c2d7d7ebde8130d2e8508434b4de63e308e38950
petr.brozek@gmail.com <petr.brozek@gmail.com>
2022-09-02 23:54:06 +0200

    Add Warning model into Application layer.

---
+++ Tickets.Application/Models/Common/Warning.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+
+namespace Tickets.Application.Models.Common;
+public class Warning
+{
+   public string Message { get; }
+
+   public Warning(string message)
+   {
+      Message = message;
+   }
+
+   public override string ToString()
+   {
+      return JsonSerializer.Serialize(this);
+   }
+}
