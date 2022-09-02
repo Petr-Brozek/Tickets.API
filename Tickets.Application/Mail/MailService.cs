87c524dd1dfb633a327f766697c7e68ade9ee609
petr.brozek@gmail.com <petr.brozek@gmail.com>
2022-09-02 23:51:07 +0200

    Fix MailService.

--- Tickets.Application/Mail/MailService.cs
+++ Tickets.Application/Mail/MailService.cs
@@ -6,24 +6,24 @@ using Tickets.Core.Models.Mail;
 namespace Tickets.Application.Mail;
 public class MailService : IMailService
 {
-    private readonly IMediator _mediator;
+   private readonly IMediator _mediator;
 
-    public MailService(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
+   public MailService(IMediator mediator)
+   {
+      _mediator = mediator;
+   }
 
-    public async Task<IList<MailMessage>> SendMailMessagesGetFailedAsync(IList<MailMessage> mailMessages)
-    {
-        var failedMailMessages = new List<MailMessage>();
-        foreach (var mailMessage in mailMessages)
-        {
-            var sendMailResult = await _mediator.Send(new SendMail(mailMessage));
-            if (sendMailResult.IsError)
-            {
-                failedMailMessages.Add(mailMessage);
-            }
-        }
-        return failedMailMessages;
-    }
+   public async Task<IEnumerable<MailMessage>> SendMailMessagesGetFailedAsync(IEnumerable<MailMessage> mailMessages)
+   {
+      var failedMailMessages = new List<MailMessage>();
+      foreach (var mailMessage in mailMessages)
+      {
+         var sendMailResult = await _mediator.Send(new SendMail(mailMessage));
+         if (sendMailResult.IsError)
+         {
+            failedMailMessages.Add(mailMessage);
+         }
+      }
+      return failedMailMessages;
+   }
 }
